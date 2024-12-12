23eed0f264ea9d533a099ea26e9a26867c3bdc22
KVNC-source <kenola700@gmail.com>
2024-12-12 08:07:15 +0800

    Rubahan pada PHP file

--- PHP/profile.php
+++ PHP/profile.php
@@ -134,35 +134,13 @@ try {
         <div class="box-container">
           <div class="box">
             <div class="flex">
-              <i class="fas fa-bookmark"></i>
-              <div>
-                <span>4</span>
-                <p>saved playlist</p>
-              </div>
-            </div>
-            <a href="#" class="inline-btn">view playlists</a>
-          </div>
-
           <div class="box">
             <div class="flex">
-              <i class="fas fa-heart"></i>
-              <div>
-                <span>33</span>
-                <p>videos liked</p>
-              </div>
-            </div>
-            <a href="#" class="inline-btn">view liked</a>
-          </div>
-
-          <div class="box">
-            <div class="flex">
-              <i class="fas fa-comment"></i>
               <div>
                 <span>12</span>
                 <p>videos comments</p>
               </div>
             </div>
-            <a href="#" class="inline-btn">view comments</a>
           </div>
         </div>
       </div>
